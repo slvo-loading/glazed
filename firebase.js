604e4d86addc4869c93826ded0108891bffeb049
slvo-loading <sydneyvo098@gmail.com>
2025-10-24 20:06:23 -0700

    initialized

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApuZiZ4N_5MC5aFKVBWjZSqwXl6J1cbaw",
+  authDomain: "glazed-b5372.firebaseapp.com",
+  projectId: "glazed-b5372",
+  storageBucket: "glazed-b5372.firebasestorage.app",
+  messagingSenderId: "1020586515551",
+  appId: "1:1020586515551:web:a51a846abcf6bfc3590fea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
